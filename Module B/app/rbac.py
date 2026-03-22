e00cdb7a8d11fa940aaf2b92aece5e50b31b9f3f
Moksh Jindal <143334002+MokshjJindal09@users.noreply.github.com>
2026-03-22 13:03:45 +0530

    Update rbac.py

--- Module B/app/rbac.py
+++ Module B/app/rbac.py
@@ -1,18 +1,3 @@
-# def is_admin(role):
-#     return role == 'Admin'
-
-# def is_student(session):
-#     return session.get('sub_role') == 'Student'
-
-# def is_staff(session):
-#     return session.get('sub_role') == 'Staff'
-
-# def get_member_id(session):
-#     return session.get('member_id')
-
-# def get_sub_id(session):
-#     return session.get('sub_id')
-
 def is_admin(role):
     """Pass session.get('role')"""
     return role == 'Admin'
